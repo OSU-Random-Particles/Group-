e635ca2fc22bc51e85e0713f499893d7dd205628
chlrkdtn2377 <65138829+chlrkdtn2377@users.noreply.github.com>
2020-05-12 12:32:01 -0700

    Update group_project Edit 3.m

--- group_project Edit 3.m
+++ group_project Edit 3.m
@@ -1,4 +1,4 @@
-clear all;
+ clear all;
 close all;
 clc;
 % Plot the circle
@@ -17,22 +17,24 @@ for j = 1:number_circle
     plot(x_circle,y_circle,'black-')
     hold on
 end
-for j = 1:500
+for j = 1:3
     x_coordinate = 0; % initialize x-cordinate
     y_coordinate = 0; % initialize y-cordinate
     Number_Step = 100; % The number of step that the particle is moving
     Step_size = 1; % The size of step when it moves
+
+
     % Angle 
     for i = 1:Number_Step
         Angle = rand*2*pi; % random angle
         % calculate x and y coorditanes to graph
-        x_coordinate = (Step_size * cos(Angle)) + x_coordinate;
-        y_coordinate = (Step_size * sin(Angle)) + y_coordinate;
+        x_coordinate(i+1) = (Step_size * cos(Angle)) + x_coordinate(i);
+        y_coordinate(i+1) = (Step_size * sin(Angle)) + y_coordinate(i);
     end
     x = x_coordinate;
     y = y_coordinate;
     % Graph each step
-    plot(x,y,'o','MarkerEdgeColor','b','MarkerFaceColor','r')
+    plot(x,y)
     hold on
     % Label the graph with the number of steps taken
     title(['Step ',num2str(i)])
@@ -42,4 +44,3 @@ for j = 1:500
     pause(0.1)
     % We don't need the circles if we don't want to plot them
 end
-
