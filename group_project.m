38e5449b8229c70a8739a584441d0730fc44c823
chlrkdtn2377 <65138829+chlrkdtn2377@users.noreply.github.com>
2020-05-20 09:51:45 -0700

    Add files via upload

--- group_project.m
+++ group_project.m
@@ -1,18 +1,50 @@
 % Group project Random Walk
+
+%% Workspace Preamble
 clear all;
 close all;
 clc;
 
-x_cordinate = 0; % x-cordinate
-y_cordinate = 0; % y-cordinate
-Number_Step = 100; % The number of step that the particle is moving
-Step_size = 1; % The size of step when it moves
+%% User Input
+prompt1 = 'How many particles do you want to observe?: ';
+Number_Particles = input(prompt1);
+%The number of step that the particle is moving
+prompt2 = 'How many steps do you want the particle(s) to move?: ';
+Number_Step = input(prompt2); 
+%The size of step when it moves
+prompt3 = 'What step size do you want the particle(s) to take?: ';
+Step_Size = input(prompt3);
 
-% Angle 
-for i = 1:Number_Step
-    Angle = rand*2*pi; % random angle
-    x_cordinate(i+1) = (Step_size * cos(Angle)) + x_cordinate(i);
-    y_cordinate(i+1) = (Step_size * sin(Angle)) + y_cordinate(i);
+%% Monte Carlo For Loop
+for j = 1:Number_Particles % Loop for # of particles
+    x_coordinate = 0; %Initialize x-cordinate
+    y_coordinate = 0; %Initialize y-cordinate
+    % Loop For Random Angle
+    for i = 1:Number_Step %Iterate to length of number of steps
+        Angle = rand*2*pi; %Create random angle
+        x_coordinate = (Step_Size * cos(Angle)) + x_coordinate; % x coordinate calc
+        y_coordinate = (Step_Size * sin(Angle)) + y_coordinate; % y coordinate calc
+    end
+    
+    x(j) = x_coordinate; %Index x coordinates
+    y(j) = y_coordinate; %Index y coordinates
+    r(j) = x(j)^2 + y(j)^2; 
+    %Graph for each step
+    figure(1)
+    plot(x,y,'o','MarkerEdgeColor','b','MarkerFaceColor','r')
+    hold on
+    title(['Step ',num2str(i)]) %Label the graph with the number of steps taken
+    axis([-20,20,-20,20]) %Set the axes so they don't continually change
+    pause(0.1) %Let the graph refresh
+   
 end
-plot(x_cordinate,y_cordinate)
-axis([-2*pi,2*pi,-2*pi,2*pi])
+
+%% Histogram for X
+figure(2)
+histogram(x)
+
+%% Standard Deviation Calculation
+t = Number_Step * Step_Size;% Time
+sigma = std(r); % Standard deviation
+D = sigma^2/(2*t); % diffusivity
+fprintf('The Diffusivity of this Brownian Distribution is : %.3f \n', D)
